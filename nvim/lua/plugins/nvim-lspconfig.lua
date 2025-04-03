fb3e5675ceb0ea3b45f295eafad802f0fc848d00
Oliver Weiß <oweiss@outlook.de>
2025-04-03 16:46:12 +0200

    switch to typescript-tools

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,16 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      tsserver = {
+        enabled = false,
+      },
+      ts_ls = {
+        enabled = false,
+      },
+      vtsls = {
+        enabled = false,
+      },
+    },
   },
 }
