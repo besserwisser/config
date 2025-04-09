db71d7758d7a4ca6689612c8a8144bf0b0ae5d48
Oliver Weiß <oweiss@outlook.de>
2025-04-09 14:30:37 +0200

    change to telescope file explorer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,22 +8,22 @@ vim.keymap.set("n", "q:", "<Nop>", { noremap = true })
 vim.keymap.set("n", "<leader>am", "<Cmd>CopilotChatModels<CR>", { desc = "Open Copilot Chat Models" })
 
 -- https://www.reddit.com/r/neovim/comments/14e59ub/i_wrote_a_function_that_moves_the_cursor_to_the/
-local ex_to_current_file = function()
-  local cur_file = vim.fn.expand("%:t")
-  vim.cmd.Ex()
+-- local ex_to_current_file = function()
+--   local cur_file = vim.fn.expand("%:t")
+--   vim.cmd.Ex()
+--
+--   local starting_line = 0 -- line number of the first file
+--   local lines = vim.api.nvim_buf_get_lines(0, starting_line, -1, false)
+--   for i, file in ipairs(lines) do
+--     if file == cur_file then
+--       vim.api.nvim_win_set_cursor(0, { starting_line + i, 0 })
+--       return
+--     end
+--   end
+-- end
 
-  local starting_line = 0 -- line number of the first file
-  local lines = vim.api.nvim_buf_get_lines(0, starting_line, -1, false)
-  for i, file in ipairs(lines) do
-    if file == cur_file then
-      vim.api.nvim_win_set_cursor(0, { starting_line + i, 0 })
-      return
-    end
-  end
-end
-
-vim.keymap.set("n", "<leader>e", ex_to_current_file, { desc = "Explore current files's directory" })
-vim.keymap.set("n", "<leader>E", "<Cmd>Explore .<CR>", { desc = "Explore current working directory" })
+-- vim.keymap.set("n", "<leader>e", ex_to_current_file, { desc = "Explore current files's directory" })
+-- vim.keymap.set("n", "<leader>E", "<Cmd>Explore .<CR>", { desc = "Explore current working directory" })
 
 vim.keymap.set(
   "n",
@@ -33,3 +33,5 @@ vim.keymap.set(
 )
 
 vim.keymap.set("n", "<leader>fo", "<Cmd>Rfinder<CR>", { desc = "Open file in Mac Finder" })
+
+vim.keymap.set("n", "<space>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,8 @@ return {
   dependencies = {
     {
       "nvim-telescope/telescope-live-grep-args.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-lua/plenary.nvim",
       -- This will not install any breaking changes.
       -- For major updates, this must be adjusted manually.
       version = "^1.0.0",
@@ -13,11 +15,20 @@ return {
 
     -- first setup telescope
     telescope.setup({
-      -- your config
+      extensions = {
+        file_browser = {
+          respect_gitignore = false,
+          hidden = {
+            file_browser = true,
+            folder_browser = true,
+          },
+        },
+      },
     })
 
     -- then load the extension
     telescope.load_extension("live_grep_args")
+    telescope.load_extension("file_browser")
   end,
   keys = {
     -- Otherwise we cannot remap
