1cb79c5e9542c8b3122c4377884fbd91f28d1d17
Oliver Weiß <oweiss@outlook.de>
2025-04-11 16:03:44 +0200

    prevent env files from formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,6 +3,15 @@ return {
   opts = {
     formatters_by_ft = {
       xml = { "xmllint" },
+      sh = function(buffnr)
+        local path = vim.api.nvim_buf_get_name(buffnr)
+        local filename = vim.fn.fnamemodify(path, ":t")
+        -- Prevent shfmt from formatting .env files
+        if filename:match("%.env$") then
+          return {}
+        end
+        return { "shfmt" }
+      end,
     },
   },
 }
