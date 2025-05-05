d4857ed75e0d0dc485ad1b8dcfed1c1e05c81661
Oliver Weiß <oweiss@outlook.de>
2025-05-05 11:05:20 +0200

    shut off no-extraneous-dependencies

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,15 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      eslint = {
+        settings = {
+          rulesCustomizations = {
+            -- https://github.com/import-js/eslint-plugin-import/issues/1913
+            { rule = "import/no-extraneous-dependencies", severity = "off" },
+          },
+        },
+      },
+    },
   },
 }
