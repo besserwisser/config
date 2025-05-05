f9cb802a93d27d4c5666dfe0bd3a46206c9a07dc
Oliver Weiß <oweiss@outlook.de>
2025-05-05 09:35:07 +0200

    default ts lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,16 +2,5 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
-    servers = {
-      tsserver = {
-        enabled = false,
-      },
-      ts_ls = {
-        enabled = false,
-      },
-      vtsls = {
-        enabled = false,
-      },
-    },
   },
 }
