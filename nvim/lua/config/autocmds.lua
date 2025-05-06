adb30b347f1b0bea378bb062b4fe3ad5366f4713
Oliver Weiß <oweiss@outlook.de>
2025-05-06 13:16:32 +0200

    disable auto formatting and diagnostics for env files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,4 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+  pattern = ".env*",
+  callback = function(e)
+    -- Disable diagnostics for .env files
+    vim.diagnostic.enable(false, { bufnr = e.buf })
+    -- Disable autoformatting for .env files
+    vim.b.autoformat = false
+  end,
+})
