ffbb83fbce657d9647f9891d0ccb0e2ff8db7a3c
Oliver Weiß <oweiss@outlook.de>
2025-05-13 10:33:13 +0200

    simplify lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,21 @@
+local icons = LazyVim.config.icons
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
     sections = {
+      lualine_c = {
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+      },
       lualine_y = { "buffers" },
       lualine_z = { "tabs" },
     },
