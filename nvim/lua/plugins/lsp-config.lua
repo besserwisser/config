fe4848a7a0e8f85d3c6475ae9e66e7aead327bce
Oliver Weiß <oweiss@outlook.de>
2025-07-21 22:40:07 +0200

    wip: undo lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,11 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
+    lazy = false,    
+		dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
     opts = {
 			ensure_installed = {
 				"lua_ls", "ts_ls"
@@ -20,19 +24,32 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
 				settings = {
-				Lua = {
-					diagnostics = {
-						-- Prevet errors of global vim object usage in config
-						globals = { 'vim' },
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								'vim',
+								'require'
+							},
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = {
+							enable = false,
+						},
 					},
-					workspace = {
-						-- Make the server aware of Neovim runtime files
-						library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
-					}
 				}
-			}
 			})
 			lspconfig.ts_ls.setup({})
+
     end,
   },
 }
