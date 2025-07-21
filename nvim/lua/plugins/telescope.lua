a2fb1628fb941c2b3d998d31744746c837da0041
Oliver Weiß <oweiss@outlook.de>
2025-07-21 19:50:05 +0200

    wip: remove ui select for now

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,32 +2,17 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = {
-    'nvim-lua/plenary.nvim',    
-		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    'nvim-telescope/telescope-ui-select.nvim', 
-		-- Useful for getting pretty icons, but requires a Nerd Font.
-		{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-  },
-  opts = {
-    extensions = {
-      ["ui-select"] = require("telescope.themes").get_dropdown(),
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
     },
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
-  -- Simplified config function, since opts is not used here
-  config = function()
-    require("telescope").load_extension("ui-select")
-		require("telescope").load_extension("fzf")
+  config = function(_, opts)
+    require("telescope").load_extension("fzf")
   end,
 }
