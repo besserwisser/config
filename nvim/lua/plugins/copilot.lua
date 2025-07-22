9a78510a17a8d519c11177d73b94e79f71b5151b
Oliver Weiß <oweiss@outlook.de>
2025-07-22 13:29:30 +0200

    remove unused agents

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,18 +3,14 @@ return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{ "github/copilot.vim" },                    -- or zbirenbaum/copilot.lua
-			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
-		build = "make tiktoken",                       -- Only on MacOS or Linux
-		opts = {
-			-- See Configuration section for options
-		},
+		build = "make tiktoken",
+		opts = {},
 		keys = {
 			{ "<leader>aa", "<cmd>CopilotChatOpen<cr>",    desc = "Copilot Open" },
-			{ "<leader>ag", "<cmd>CopilotChatAgents<cr>",  desc = "Copilot Chat Agents" },
 			{ "<leader>ap", "<cmd>CopilotChatPrompts<cr>", desc = "Copilot Chat Prompts" },
 		},
-		-- See Commands section for default commands if you want to lazy load on them
 	},
 }
