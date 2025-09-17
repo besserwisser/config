56960e561908d9465db0d2e8f7ea0b1d44acb6ac
Oliver Weiß <oweiss@outlook.de>
2025-09-17 14:53:40 +0200

    refactor markdown

--- nvim/lua/plugins/preview.lua
+++ nvim/lua/plugins/preview.lua
@@ -1,26 +1,26 @@
-vim.pack.add({
-	{
-		src = "https://github.com/iamcco/markdown-preview.nvim",
-		-- version = "v0.0.10"
-	},
-})
+vim.pack.add({ "https://github.com/iamcco/markdown-preview.nvim" })
+
+local function notify_done(msg)
+	vim.schedule(function()
+		vim.notify(msg, vim.log.levels.INFO)
+	end)
+end
+
+local function install_and_build(path)
+	vim.system({ "npm", "--yes", "install" }, { cwd = path }, function()
+		notify_done("markdown-preview npm install done")
+		vim.system({ "npm", "--yes", "run", "build" }, { cwd = path }, function()
+			notify_done("markdown-preview npm run build done")
+		end)
+	end)
+end
 
 vim.api.nvim_create_autocmd({ "PackChanged" }, {
 	group = vim.api.nvim_create_augroup("MarkdownPreviewUpdated", { clear = true }),
 	callback = function(args)
 		local data = args.data
-
 		if data.spec and data.spec.name == "markdown-preview.nvim" and data.kind == "update" then
-			vim.system({ "npm", "--yes", "install" }, { cwd = data.path }, function()
-				vim.schedule(function()
-					vim.notify("markdown-preview npm install done", vim.log.levels.INFO)
-				end)
-				vim.system({ "npm", "--yes", "run", "build" }, { cwd = data.path }, function()
-					vim.schedule(function()
-						vim.notify("markdown-preview npm run build done", vim.log.levels.INFO)
-					end)
-				end)
-			end)
+			install_and_build(data.path)
 		end
 	end,
 })
