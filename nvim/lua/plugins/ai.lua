c5859533d9474824cdb44cc88bd1df54213a132a
Oliver Weiß <oweiss@outlook.de>
2025-10-01 12:02:21 +0200

    refactor return

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -8,11 +8,11 @@ sidekick.setup()
 vim.keymap.set({ "i", "n" }, "<tab>", function()
 	-- if there is a next edit, jump to it, otherwise apply it if any
 	if require("sidekick").nes_jump_or_apply() then
-		return ""
+		return
 	end
 	-- if you are using Neovim's native inline completions
 	if vim.lsp.inline_completion.get() then
-		return ""
+		return
 	end
 	-- any other things (like snippets) you want to do on <tab> go here.
 	-- fall back to normal tab
