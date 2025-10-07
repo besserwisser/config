49de2a93bf9b0679198b92ea289c5b8a64b31a02
Oliver Weiß <oweiss@outlook.de>
2025-10-07 14:31:18 +0200

    auto build markdown on install

--- nvim/lua/plugins/preview.lua
+++ nvim/lua/plugins/preview.lua
@@ -1,5 +1,3 @@
-vim.pack.add({ "https://github.com/iamcco/markdown-preview.nvim" })
-
 local function notify_done(msg)
 	vim.schedule(function()
 		vim.notify(msg, vim.log.levels.INFO)
@@ -7,10 +5,11 @@ local function notify_done(msg)
 end
 
 local function install_and_build(path)
-	vim.system({ "npm", "--yes", "install" }, { cwd = path }, function()
-		notify_done("markdown-preview npm install done")
-		vim.system({ "npm", "--yes", "run", "build" }, { cwd = path }, function()
-			notify_done("markdown-preview npm run build done")
+	vim.notify("Installing and building markdown-preview.nvim...")
+	vim.system({ "yarn", "install" }, { cwd = path }, function()
+		notify_done("markdown-preview yarn install done")
+		vim.system({ "yarn", "run", "build" }, { cwd = path }, function()
+			notify_done("markdown-preview yarn run build done")
 		end)
 	end)
 end
@@ -19,8 +18,13 @@ vim.api.nvim_create_autocmd({ "PackChanged" }, {
 	group = vim.api.nvim_create_augroup("MarkdownPreviewUpdated", { clear = true }),
 	callback = function(args)
 		local data = args.data
-		if data.spec and data.spec.name == "markdown-preview.nvim" and data.kind == "update" then
-			install_and_build(data.path)
+		if data.spec and data.spec.name == "markdown-preview.nvim" then
+			if data.kind == "update" or data.kind == "install" then
+				install_and_build(data.path)
+			end
 		end
 	end,
 })
+
+-- Need to set it up here to ensure the autocmd is created before the plugin is installed/updated
+vim.pack.add({ "https://github.com/iamcco/markdown-preview.nvim" })
