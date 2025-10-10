101446540e9a25288214a410a88a40da2b3238db
Oliver Weiß <oweiss@outlook.de>
2025-10-10 15:39:33 +0200

    disable netrw

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
 local utils = require("config.utils")
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- disable mouse support
 vim.o.mouse = ""
 
