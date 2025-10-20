5b2ff7db241b287f16d5e177ddb64aba421a4a21
Oliver Weiß <oweiss@outlook.de>
2025-10-20 11:25:56 +0200

    make cursor line only visible in active window

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -15,3 +15,24 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	callback = utils.show_tip,
 	desc = "Show custom dashboard on startup",
 })
+
+-- Show cursor line only in the active window to make clear which window is active
+local cursorline_group = vim.api.nvim_create_augroup("CursorLineOnlyInActiveWindow", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+	group = cursorline_group,
+	pattern = "*",
+	callback = function()
+		vim.wo.cursorline = true -- 'vim.wo' is for window-local options
+	end,
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+	group = cursorline_group,
+	pattern = "*",
+	callback = function()
+		vim.wo.cursorline = false
+	end,
+})
