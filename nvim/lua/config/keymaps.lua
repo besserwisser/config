3b4130d3f675596799cb80a58d4c792f876824dd
Oliver Weiß <oweiss@outlook.de>
2025-10-21 14:16:13 +0200

    undo arrow keys hiding

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,2 @@
 -- Clear highlights on search when pressing Esc in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- disable arrow keys in all modes
-vim.keymap.set({ "n", "i", "v", "c" }, "<Up>", "<Nop>")
-vim.keymap.set({ "n", "i", "v", "c" }, "<Down>", "<Nop>")
-vim.keymap.set({ "n", "i", "v", "c" }, "<Left>", "<Nop>")
-vim.keymap.set({ "n", "i", "v", "c" }, "<Right>", "<Nop>")
