9c1f9120f9383abb92946c870109b913a4f03147
Oliver Weiß <oweiss@outlook.de>
2025-11-03 09:11:07 +0100

    remove sidekick

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,33 +1,3 @@
-vim.pack.add({
-	"https://github.com/folke/sidekick.nvim",
-})
-
-local sidekick = require("sidekick")
-sidekick.setup()
-
-vim.keymap.set({ "i", "n" }, "<tab>", function()
-	-- if there is a next edit, jump to it, otherwise apply it if any
-	if require("sidekick").nes_jump_or_apply() then
-		return
-	end
-	-- if you are using Neovim's native inline completions
-	if vim.lsp.inline_completion.get() then
-		return
-	end
-	-- any other things (like snippets) you want to do on <tab> go here.
-	-- fall back to normal tab
-	return "<tab>"
-end, { expr = true })
-vim.keymap.set({ "n", "v" }, "<leader>aa", function()
-	require("sidekick.cli").toggle({ focus = true, name = "copilot" })
-end)
-vim.keymap.set({ "n", "v" }, "<leader>ap", function()
-	require("sidekick.cli").prompt()
-end)
-vim.keymap.set("t", "<C-w>p", function()
-	require("sidekick.cli").focus({ focus = false })
-end)
-
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("EnableInlineCompletion", { clear = true }),
 	callback = function()
