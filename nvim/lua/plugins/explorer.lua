78cfc2ecad3ed5199d6efe061f5ee0c0750ca39b
Oliver Weiß <oweiss@outlook.de>
2025-12-01 13:36:32 +0100

    oil config

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -3,11 +3,26 @@ vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
 local oil = require("oil")
 oil.setup({
 	keymaps = {
-		["<Esc>"] = "actions.close",
-		mode = "n",
+		-- Close Oil
+		["<Esc>"] = {
+			"actions.close",
+			mode = "n",
+		},
+		-- Preview scrolling
+		["<C-f>"] = "actions.preview_scroll_down",
+		["<C-b>"] = "actions.preview_scroll_up",
 	},
 })
 
+-- Keymap to toggle Oil float
 vim.keymap.set("n", "<leader>e", function()
 	oil.toggle_float()
 end, { desc = "Toggle Oil float" })
+
+-- Open preview window when entering Oil
+vim.api.nvim_create_autocmd("User", {
+	pattern = "OilEnter",
+	callback = function()
+		oil.open_preview()
+	end,
+})
