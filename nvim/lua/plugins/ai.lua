4423cc4d75b9585400c3aae8d4d7b1ac2d224103
Oliver Weiß <oweiss@outlook.de>
2025-12-05 09:05:17 +0100

    deactivate AI in leetcode

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -7,6 +7,11 @@ end, { expr = true, desc = "Accept the current inline completion" })
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("EnableInlineCompletion", { clear = true }),
 	callback = function()
+		-- Disable inline completion for leetcode project
+		local cwd = vim.fn.getcwd()
+		if cwd:match("Dev/Private/leetcode") then
+			return
+		end
 		-- Enable LLM-based inline completion
 		vim.lsp.inline_completion.enable(true)
 		vim.keymap.set("i", "<C-;>", function()
