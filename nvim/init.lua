1890eda210d85728e0ac1c6fba3da638f7ee7127
Oliver Weiß <oweiss@outlook.de>
2026-03-09 13:55:09 +0100

    add comment for packadd

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,6 +19,9 @@ require("plugins.ai")
 require("plugins.preview")
 require("plugins.keymap-helper")
 
+-- Use cfilter with the command :Cfilter to filter the quickfix list. For example `:Cfilter error` will show only the errors in the quickfix list, while `:Cfilter! error` will show anything but the errors.
 vim.cmd("packadd cfilter")
+-- Use undotree with the command :Undotree to visualize the undo history. This is useful for understanding the changes made to a file and for easily reverting to previous states.
 vim.cmd("packadd nvim.undotree")
+-- Use `git difftool -d main` to compare the current branch with the main branch. This is useful for reviewing changes before committing or for understanding the differences between branches.
 vim.cmd("packadd nvim.difftool")
