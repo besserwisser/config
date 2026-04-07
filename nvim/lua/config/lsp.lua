73a0fd3253e5bbb8992491e6d39bf9f57c5f4891
Oliver Weiß <oweiss@outlook.de>
2026-04-07 07:22:04 +0200

    speedup lua start

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -15,7 +15,7 @@ vim.lsp.config("emmylua_ls", {
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.api.nvim_get_runtime_file("lua", true),
 			},
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
